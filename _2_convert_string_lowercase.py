c5d3e676140c20b809d180789dea1bcd00e3a578
Rakib <rakib@Rakibs-MacBook-Pro.local>
2024-03-18 00:04:59 +0600

    2.convert string into lowercase

---
+++ _2_convert_string_lowercase.py
@@ -0,0 +1,7 @@
+def lowerCase(input_string):
+  return input_string.lower()
+
+input_string = input("enter string :")
+lower_string = lowerCase(input_string)
+print("Convert lower string : " + lower_string)
+  
